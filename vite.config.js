da1648a31670cd8476242aab56d4b56bc9250ccd
jhopkins78 <63133048+jhopkins78@users.noreply.github.com>
2024-12-04 22:31:20 -0800

    Update Vite and Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './static',
+  root: '.',
   base: '/',
   build: {
-    outDir: '../static/dist',
-    assetsDir: '',
+    outDir: 'dist',
+    assetsDir: 'assets',
     manifest: true,
     rollupOptions: {
       input: './static/js/main.jsx',
